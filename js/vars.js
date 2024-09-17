b6f98384ea64ff0aba80cbe4884e2bbd03fbc65c
DimaTurkey <123176664+DimaTurkey@users.noreply.github.com>
2024-09-17 17:25:01 +0300

    interested komit

---
+++ js/vars.js
@@ -0,0 +1,39 @@
+let mydimasage = 13; 
+console.log(mydimasage); 
+// My age
+let mydmutroname = "Діма";
+console.log(mydmutroname);
+// My name
+let ITDimaStudent = true;
+console.log(ITDimaStudent);
+// I am ITStudent
+let SadStudent = false; 
+console.log(SadStudent);
+// I am not sad student
+let mySecretandChitate = "Добре,добре моі улюблені передачі це Дизель Шоу, Баранчик Шон, Міа та я, Нікому не кажіть"; // Заміни на свою улюблену цитату
+console.log(mySecretandChitate);
+// Мій секрет і моя цитата :)
+let myNumber = 9 - 3 * 4 / 2; 
+myNumber = myNumber + 10; 
+console.log(myNumber);
+// Джава скрипт арифметика і алгебра
+let myNull = null;
+console.log(myNull);
+// Порожнеча
+let userName = prompt("Введіть своє ім'я та прізвище. Далі: введіть пароль від банківської карти та пароль від аккауту");
+alert("Вітаю, " + userName + "Компанія Діма та індики взламала вас!");
+// Мій міні жарт
+let userResponse = confirm("Ви впевнені що погоджуєтеся з моєю пропозицією?");
+if (userResponse) {
+  alert("Дякую за підтвердження за інформацію про вас. Мої індики взламали вас!");
+} else {
+  alert("Дію відмінено і компютер вірус не підхопить :)");
+}
+// Ну як жарт
+alert("Увага! Ця дія може бути небезпечною для вашого компютера.");
+let userSurprise = confirm("Ви впевнені, що хочете продовжити бо вороття не буде?");
+if (userSurprise) {
+  alert("Дякую за підтвердження! На вашому аккауні вірус і Гугл заблокує його на 2 тижні задля безпеки інших коритувачів");
+} else {
+  alert("Дію відмінено і аккаунт не потрапить на карантин!");
+}
